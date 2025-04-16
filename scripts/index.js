5a773ced1fbc13d98e4bc7aff9514d07b63d859a
Alexander Starodubov <as@plinar.com>
2025-04-16 13:27:25 +0300

    fix: correct card create and delete

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,12 @@ const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function createCard(arrayElement, deleteCard) {
 
-    // склонировали структуру шаблона
-    const newCard = cardTemplate.cloneNode(true);
+    // склонировали структуру шаблона. 
+    const newCardFragment = cardTemplate.cloneNode(true);
+    
+    // в newCard находится временный DocumentFragment. Он очистится после append(). 
+    // буду напрямую работать с .card, а не с временным DocumentFragment
+    const newCard = newCardFragment.querySelector('.card');
 
     // у нового элемента нашли Title, Image, DeleteButton
     const cardTitle = newCard.querySelector('.card__title');
@@ -18,20 +22,18 @@ function createCard(arrayElement, deleteCard) {
     // в новый элемент записали значения из параметра функции arrayElement
     cardTitle.textContent = arrayElement.name;
     cardImg.src = arrayElement.link;
+    cardImg.alt = `Фотография места: ${arrayElement.name}`; 
 
     // в новом элементе к кнопке DeleteButton добавили обработчик. 
     // в deleteCard передаётся событие дефолтно из которого можно достать карту (target.closest)
-    deleteButton.addEventListener('click', deleteCard);
+    deleteButton.addEventListener('click', () => deleteCard (newCard));
 
     // вернули созданный заполненный элемент
     return newCard;
 }
 
 // @todo: Функция удаления карточки
-function removeCard(event) {
-    const card = event.target.closest('.card');
-    const cardTitle = card.querySelector('.card__title');
-    console.log('Удалил', cardTitle.textContent);
+function removeCard(card) {
     card.remove();
 }
 

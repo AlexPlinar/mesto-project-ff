b6d99d23ec5bb41123e706c26f6cb9ac730c8188
Alexander Starodubov <as@plinar.com>
2025-06-03 19:53:47 +0300

    refactor: add build and dev webpack mode, change project structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
@@ -9,7 +11,7 @@ function createCard(arrayElement, deleteCard) {
 
     // склонировали структуру шаблона. 
     const newCardFragment = cardTemplate.cloneNode(true);
-    
+
     // в newCard находится временный DocumentFragment. Он очистится после append(). 
     // буду напрямую работать с .card, а не с временным DocumentFragment
     const newCard = newCardFragment.querySelector('.card');
@@ -22,11 +24,11 @@ function createCard(arrayElement, deleteCard) {
     // в новый элемент записали значения из параметра функции arrayElement
     cardTitle.textContent = arrayElement.name;
     cardImg.src = arrayElement.link;
-    cardImg.alt = `Фотография места: ${arrayElement.name}`; 
+    cardImg.alt = `Фотография места: ${arrayElement.name}`;
 
     // в новом элементе к кнопке DeleteButton добавили обработчик. 
     // в deleteCard передаётся событие дефолтно из которого можно достать карту (target.closest)
-    deleteButton.addEventListener('click', () => deleteCard (newCard));
+    deleteButton.addEventListener('click', () => deleteCard(newCard));
 
     // вернули созданный заполненный элемент
     return newCard;
@@ -45,3 +47,41 @@ function pushCards() {
 }
 
 pushCards();
+
+const editButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('.popup_type_edit');
+
+const newCardButton = document.querySelector('.profile__add-button');
+const newCardPopup = document.querySelector('.popup_type_new-card');
+
+const places = document.querySelector('.places__list');
+const imagePopup = document.querySelector('.popup_type_image');
+const image = imagePopup.querySelector('.popup__image');
+
+const popups = document.querySelectorAll('.popup');
+
+editButton.addEventListener('click', () => {
+    editPopup.classList.toggle('popup_is-opened');
+});
+
+newCardButton.addEventListener('click', () => {
+    newCardPopup.classList.toggle('popup_is-opened');
+});
+
+places.addEventListener('click', (evt) => {
+    imagePopup.classList.toggle('popup_is-opened');
+    image.src = evt.target.src;
+    image.alt = evt.target.alt;
+    
+    const card = evt.target.closest('.card');
+    const caption = card.querySelector('.card__title');
+    imagePopup.querySelector('.popup__caption').textContent = caption.textContent;
+});
+
+popups.forEach((elem) => {
+    elem.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup_is-opened')) {
+            elem.classList.toggle('popup_is-opened');
+        }
+    })
+})
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'  // все картинки в папку dist/images/
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images'), 
+          to: 'images' 
+        }
+      ]
+    })
+
+  ]
+}

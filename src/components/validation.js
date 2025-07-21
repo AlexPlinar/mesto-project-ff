0899caf64870471aba5a472d7c057381e9d93aa2
Alexander Starodubov <as@plinar.com>
2025-07-21 21:42:26 +0300

    feat: add validation color and messages

--- src/components/validation.js
+++ src/components/validation.js
@@ -13,28 +13,40 @@ function setEventListeners(formElement, settings) {
 
     inputs.forEach(inputElement => {
         inputElement.addEventListener('input', () => {
-            isValid(formElement, inputElement, settings);
+            checkInputValidity(formElement, inputElement, settings);
         });
     })
 }
 
-function isValid(formElement, inputElement, settings) {
-    if (!inputElement.validity.valid) {
-        console.log('НЕ ВАЛИДНО');
-        inputElement.setCustomValidity('КАСТОМНОЕ СООБЩЕНИЕ')
-        showInputError(formElement, inputElement, settings); // надо показать
+function checkInputValidity(formElement, inputElement, settings) {
+    const regex = /^[a-zа-яё\s-]+$/i;
+    const inputValue = inputElement.value.trim();
+
+    if (inputValue && !regex.test(inputValue)) {
+        inputElement.setCustomValidity('Поле может содержать только латинские и кириллические буквы, знаки дефиса и пробелы');
     } else {
-        console.log('ВАЛИДНО');
+        inputElement.setCustomValidity('');
+    }
+
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, settings);
+    } else {
         hideInputError(formElement, inputElement, settings);
     }
 }
 
 function showInputError(formElement, inputElement, settings) {
-
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(settings.inputErrorClass); // добавляем класс на INPUT для стилизации рамок
+    errorElement.classList.add(settings.errorClass); // добавляем класс на SPAN для вывода текста
+    errorElement.textContent = inputElement.validationMessage;
 }
 
 function hideInputError(formElement, inputElement, settings) {
-    
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(settings.inputErrorClass);
+    errorElement.classList.remove(settings.errorClass);
+    errorElement.textContent = '';
 }
 
 export function clearValidation(settings) {
